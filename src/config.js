b111c22a3dd6dcb1dd91a89cc78b4e57185b90b0
Jobal <jbaldwije@gmail.com>
2020-06-09 20:52:49 -0400

    connected to api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT:'https://radiant-garden-18168.herokuapp.com'
+    API_ENDPOINT:'https://radiant-caverns-61441.herokuapp.com'
 }
 //http://localhost:9000
 //https://radiant-garden-18168.herokuapp.com
